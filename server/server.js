088095ca250db045e334aba3e1cb98bae4ba6501
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-20 18:52:17 +0500

    listen for local port or if its uploaded to cloud then listen the one provided by cloud

--- server/server.js
+++ server/server.js
@@ -2,3 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
+
+const app = express()
+const port = process.env.PORT() || 4000
