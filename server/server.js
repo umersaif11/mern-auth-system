826a6d228d6e5dedb7f4c2d5dd4c76640e262a00
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-23 16:47:34 +0500

    So that incoming response get parsed if json, looking for cookies or have credentials/cookies, we need to use respective packages

--- server/server.js
+++ server/server.js
@@ -5,3 +5,7 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 const port = process.env.PORT() || 4000
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials: true}))
