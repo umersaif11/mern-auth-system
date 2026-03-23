ea694fab751a666128c1f11b1f8e143240b9801f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-23 16:54:39 +0500

    Listen to express server at port number and send the get response for the home route

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ import 'dotenv/config'
 import cookieParser from 'cookie-parser'
 
 const app = express()
-const port = process.env.PORT() || 4000
+const port = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({credentials: true}))
+
+app.get('/', (req, res) => {
+    res.send('API working')
+})
+
+app.listen(port, () => {
+    console.log(`Server started on Port:${port}`)
+})
