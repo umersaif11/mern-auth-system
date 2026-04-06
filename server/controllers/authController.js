17ab3d5eca25857744ff425abd0acff11feb8059
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-06 16:11:26 +0500

    Make a function to register a user, if any detail is missing send the response to frontend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,5 +2,10 @@
 
 export const register = (req, res) => {
     const {name, email, password} = req.body
-    
+    if(!name || !email ||!password) {
+        return res.json({
+            success: false,
+            message: 'Missing Details'
+        })
+    }
 }
