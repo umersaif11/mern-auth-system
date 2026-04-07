5ed5a555396c02d309da31191a75e9a17d0a8706
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-07 17:02:43 +0500

    if password doesnt match send a message and if match send a token to the user browser

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -70,6 +70,27 @@ export const login = async (req, res) => {
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch) {
+            return res.json({
+                success: false,
+                message: 'Invalid Password'
+            })
+        }
+
+        const token  = jwt.sign(
+            {id: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: '7d'}
+        )
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none'
+            : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
     } catch (error) {
         return res.json({success: false, message: error.message})
     }
