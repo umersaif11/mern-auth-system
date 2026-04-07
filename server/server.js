10252fd605b3016cac32a0589abd0a5167a50afb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-07 20:27:44 +0500

    Put auth routes from authRoutes file into server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 import cookieParser from 'cookie-parser'
 
 import connectDB from './config/mongodb.js'
+import { authRouter } from './routes/authRoutes.js'
+
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -13,9 +15,11 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({credentials: true}))
 
+// API Endpoints
 app.get('/', (req, res) => {
     res.send('API working')
 })
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => {
     console.log(`Server started on Port:${port}`)
