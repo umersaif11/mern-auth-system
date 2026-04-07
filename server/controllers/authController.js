5d58ea6610586b3cca24964e5c295382c9e6e7c0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-07 16:16:20 +0500

    Ensure the cookie is sent visa secure http in production, and cross site cookie sending is active in production and age of the cookie expiry

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,7 +35,13 @@ export const register = async (req, res) => {
             {expiresIn: '7d'}
         )
 
-        res.cookie('token', token)
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none'
+            : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
     } catch (error) {
         res.json({
             success: false,
