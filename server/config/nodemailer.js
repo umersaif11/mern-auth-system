1ac16168310c592a19a158b7881ed9f3abc92523
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-08 19:56:40 +0500

    Add the details in the transporter func for nodemailer

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,6 +1,12 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    auth: {
+        user: process.env.SMPTP_USER,
+        pass: process.env.SMPTP_PASS
+    }
 })
 
 export default transporter
