0cae6355ddff61f3cbfcaac8e8f9a4997c837ca1
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-09 15:46:11 +0500

    Send the email in the register controller function after sending the cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import userModel from '../models/userModel.js'
+import transporter from '../config/nodemailer.js'
 
 export const register = async (req, res) => {
     const {name, email, password} = req.body
@@ -50,8 +51,12 @@ export const register = async (req, res) => {
         const mailOptions = {
             from: process.env.SENDER_EMAIL,
             to: email,
-            subject: 'Welcome to Greatstack'
+            subject: 'Welcome to Greatstack',
+            text: `Welcome to Greatstack website. Your account has been created
+            with email id: ${email}.`
         }
+
+        await transporter.sendMail(mailOptions)
 
         return res.json({success: true})
 
