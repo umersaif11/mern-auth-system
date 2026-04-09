c0f0f15cbe5a15fac2f4275d645ddf9a52a27590
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-09 16:22:08 +0500

    Create a controller function to send verification otp to the email

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -132,3 +132,15 @@ export const logout = async (req, res) => {
         return res.json({success: false, message: error.message})
     }
 }
+
+//Send Verification OTP to the User's Email
+export const sendVerifyOtp = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
