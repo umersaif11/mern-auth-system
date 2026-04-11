8727d9049d1da9e8a70f36004c29814039a2d7b7
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-11 19:24:57 +0500

    Get token from cookie and if token not found then return false

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,5 +1,21 @@
 import jwt from "jsonwebtoken"
 
 const userAuth = async (req, res) => {
-    
+    const {token} = req.cookies
+
+    if(!token) {
+        return res.json({
+            success: false,
+            message: 'Not Authorized. Login Again.'
+        })
+    }
+
+    try {
+        
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
