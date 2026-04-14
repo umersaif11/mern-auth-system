84ca7ae4084630924b3a0b2b77f917101ddb90ba
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-14 19:34:05 +0500

    Fixed a bug and put the route in server.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel"
+import userModel from "../models/userModel.js"
 
 export const getUserData = async (req, res) => {
     try {
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 
 import connectDB from './config/mongodb.js'
 import { authRouter } from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 
 const app = express()
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
     res.send('API working')
 })
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, () => {
     console.log(`Server started on Port:${port}`)
