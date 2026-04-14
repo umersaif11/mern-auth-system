d20f04e5a4e302557a51d8938c6d4fefaaebbc5b
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-14 16:12:34 +0500

    Make the api end point routes for the reset password

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { isAuthenticated, login, logout, register, sendVerifyOtp, verifyEmail } from '../controllers/authController.js'
+import { isAuthenticated, login, logout, register, resetPassword, 
+sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js'
 import userAuth from '../middleware/userAuth.js'
 
 const authRouter = express.Router()
@@ -10,5 +11,7 @@ authRouter.post('/logout', logout)
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp)
 authRouter.post('/verify-account', userAuth, verifyEmail)
 authRouter.post('/is-auth', userAuth, isAuthenticated)
+authRouter.post('/send-reset-otp', sendResetOtp)
+authRouter.post('/reset-password', resetPassword)
 
 export {authRouter}
