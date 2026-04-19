0e82ca8fc7c4c0e34786e477be1294087ed8f591
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-19 19:19:08 +0500

    Create a header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div>
+        <img src={assets.header_img} 
+        alt=""
+        className='' 
+        />
+    </div>
+  )
+}
+
+export default Header
