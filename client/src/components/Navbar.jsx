cbff6a86f94f5aba9ebbc69f6aa714718a9eb4ac
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-19 15:08:08 +0500

    Create a navbar component and import assets object from assets folder

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src={ assets.logo } alt="" />
+    </div>
+  )
+}
+
+export default Navbar
