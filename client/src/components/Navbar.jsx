824dbb58a0300c3b068a99e837534d9b7e0c607f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-19 17:12:38 +0500

    Add navbar to the homepage and add css styles in the parent div in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
+    <div
+    className='w-full flex justify-between items-center p-4 sm:p-6
+    sm:px-24 absolute top-0'>
         <img 
         src={ assets.logo } 
         alt=""
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function Home() {
   return (
     <div>
-        <h1>Home page</h1>
+        <Navbar/>
     </div>
   )
 }
