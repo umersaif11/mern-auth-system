1f0d7d7e7a3392ff9472a2bd42c4013d7acb428b
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-20 16:23:44 +0500

    Add the header in the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
 function Home() {
   return (
     <div>
         <Navbar/>
+        <Header/>
     </div>
   )
 }
