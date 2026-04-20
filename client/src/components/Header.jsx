e36fc7f0438dca5c68e00acd3e5c3334fe0e0c6d
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-20 16:22:20 +0500

    In header add the images and h1

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,8 +6,12 @@ const Header = () => {
     <div>
         <img src={assets.header_img} 
         alt=""
-        className='' 
+        className='w-36 h-36 rounded-full mb-6' 
         />
+        <h1>
+          Hey Developer <img src={assets.hand_wave} 
+          alt="" className='w-8 aspect-square' />
+        </h1>
     </div>
   )
 }
