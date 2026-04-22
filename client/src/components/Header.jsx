97f8488898a29ffeb8a68bb089f2625e0b0dae48
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-22 19:29:55 +0500

    Make the main parent div to be flexbox and flex direction of column...

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,18 +3,23 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center mt-20 px-4 text-center
+    text-gray-800'>
         <img src={assets.header_img} 
         alt=""
         className='w-36 h-36 rounded-full mb-6' 
         />
-        <h1>
+
+        <h1 className='flex items-center gap-2 text-xl sm:text-3xl
+        font-medium mb-2'>
           Hey Developer <img src={assets.hand_wave} 
           alt="" className='w-8 aspect-square' />
         </h1>
+        
         <h2>Welcome to our app</h2>
         <p>Let's start with a quick product tour and we will have you
         up and running in no time!</p>
+        <button>Get Started</button>
     </div>
   )
 }
