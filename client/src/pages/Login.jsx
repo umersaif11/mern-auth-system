eff02f1f945efdc9211b13fc2ae3ef7a0375267c
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-23 21:03:46 +0500

    Add the image, heading and paragraph in the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const Login = () => {
+
+  const [state, setState] = useState('Sign Up')
+
   return (
     <div>
-        <h1>Login Page</h1>
+        <img src={assets.logo} alt="" className='absolute left-5 sm:left-20
+        top-5 w-28 sm:w-32 cursor-pointer'/>
+        <div>
+          <h2>{state === 'Sign Up' ? 'Create your account' : 
+          'Login to your account!'}</h2>
+          <p>{state === 'Sign Up' ? 'Create your account' : 
+          'Login to your account!'}</p>
+        </div>
     </div>
   )
 }
