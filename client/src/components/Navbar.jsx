b716de0354000ae61053b1bbbad6b058b35882a0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-23 20:38:24 +0500

    When user clicks on the login button go to the login page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div
     className='w-full flex justify-between items-center p-4 sm:p-6
@@ -12,6 +16,7 @@ const Navbar = () => {
         className='w-28 sm:w-32'
         />
         <button
+        onClick={() => navigate('/login')}
         className='flex items-center gap-2 border border-gray-500
         rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100
         transition-all'>
