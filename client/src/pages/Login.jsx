a0d5c227c4c90678dce31601d42199586f9c8bce
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-23 21:18:18 +0500

    Add image icon and input text field

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,13 +8,25 @@ const Login = () => {
   return (
     <div className='flex justify-center items-center min-h-screen px-6
     sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
+
         <img src={assets.logo} alt="" className='absolute left-5 sm:left-20
         top-5 w-28 sm:w-32 cursor-pointer'/>
+
         <div>
           <h2>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
+
           <p>{state === 'Sign Up' ? 'Create your account' : 
           'Login to your account!'}</p>
+
+          <form>
+            <div>
+              <img src={assets.person_icon} alt="" />
+              <input type="text" placeholder="Full Name"
+              required/>
+            </div>
+          </form>
         </div>
+
     </div>
   )
 }
