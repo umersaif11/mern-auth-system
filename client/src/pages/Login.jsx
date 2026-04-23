e68765929a350913482ee51f7de940d37992ba4d
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-23 21:11:10 +0500

    Make the main div flexbox and also add the background linear gradient

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,12 +6,12 @@ const Login = () => {
   const [state, setState] = useState('Sign Up')
 
   return (
-    <div>
+    <div className='flex justify-center items-center min-h-screen px-6
+    sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
         <img src={assets.logo} alt="" className='absolute left-5 sm:left-20
         top-5 w-28 sm:w-32 cursor-pointer'/>
         <div>
-          <h2>{state === 'Sign Up' ? 'Create your account' : 
-          'Login to your account!'}</h2>
+          <h2>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
           <p>{state === 'Sign Up' ? 'Create your account' : 
           'Login to your account!'}</p>
         </div>
