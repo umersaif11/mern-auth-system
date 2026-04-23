732a19e57b977fbb9df0e567896397b7718f755c
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-23 21:22:55 +0500

    Make the main div flexbox, add padding and make it look good as well as input box

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,9 +19,10 @@ const Login = () => {
           'Login to your account!'}</p>
 
           <form>
-            <div>
+            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+            rounded-full bg-[#333A5C]'>
               <img src={assets.person_icon} alt="" />
-              <input type="text" placeholder="Full Name"
+              <input className='bg-transparent outline-none' type="text" placeholder="Full Name"
               required/>
             </div>
           </form>
