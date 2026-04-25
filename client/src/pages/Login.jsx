816b1fdcb695f668fd4d96818b29f986f56d8acf
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-25 16:38:20 +0500

    Add other input fields for password and email

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,10 +14,14 @@ const Login = () => {
 
         <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full
         sm:w-96 text-indigo-300 text-sm'>
-          <h2>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
+          <h2 className='text-3xl font-semibold text-white text-center mb-3'>
+            {state === 'Sign Up' ? 'Create account' : 'Login'}
+          </h2>
 
-          <p>{state === 'Sign Up' ? 'Create your account' : 
-          'Login to your account!'}</p>
+          <p className='text-center text-sm mb-6'>
+            {state === 'Sign Up' ? 'Create your account' : 
+          'Login to your account!'}
+          </p>
 
           <form>
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
@@ -25,6 +29,20 @@ const Login = () => {
               <img src={assets.person_icon} alt="" />
               <input className='bg-transparent outline-none' 
               type="text" placeholder="Full Name" required/>
+            </div>
+
+            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+            rounded-full bg-[#333A5C]'>
+              <img src={assets.mail_icon} alt="" />
+              <input className='bg-transparent outline-none' 
+              type="email" placeholder="Email id" required/>
+            </div>
+
+            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+            rounded-full bg-[#333A5C]'>
+              <img src={assets.lock_icon} alt="" />
+              <input className='bg-transparent outline-none' 
+              type="password" placeholder="Password" required/>
             </div>
           </form>
         </div>
