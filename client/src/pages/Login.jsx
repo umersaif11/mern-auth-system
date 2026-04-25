317d83d3300484547438d1414a8d87a2bae19ef4
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-25 16:24:12 +0500

    Make the login div have bg color, roundedness, shadow and width

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,12 +7,13 @@ const Login = () => {
 
   return (
     <div className='flex justify-center items-center min-h-screen px-6
-    sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
+    sm:px-0 text-white bg-linear-to-br from-blue-200 to-purple-400'>
 
         <img src={assets.logo} alt="" className='absolute left-5 sm:left-20
         top-5 w-28 sm:w-32 cursor-pointer'/>
 
-        <div>
+        <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full
+        sm:w-96 text-indigo-300 text-sm'>
           <h2>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
 
           <p>{state === 'Sign Up' ? 'Create your account' : 
@@ -22,8 +23,8 @@ const Login = () => {
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
             rounded-full bg-[#333A5C]'>
               <img src={assets.person_icon} alt="" />
-              <input className='bg-transparent outline-none' type="text" placeholder="Full Name"
-              required/>
+              <input className='bg-transparent outline-none' 
+              type="text" placeholder="Full Name" required/>
             </div>
           </form>
         </div>
