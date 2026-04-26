aa7b9907dc82acff4987eb20fdb1bcbb38c13139
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-26 16:16:26 +0500

    Whenever you change name, email or password that should be saved in a variable

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,6 +4,9 @@ import { assets } from '../assets/assets'
 const Login = () => {
 
   const [state, setState] = useState('Sign Up')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   return (
     <div className='flex justify-center items-center min-h-screen px-6
@@ -29,7 +32,10 @@ const Login = () => {
                 <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
                 rounded-full bg-[#333A5C]'>
                   <img src={assets.person_icon} alt="" />
-                  <input className='bg-transparent outline-none' 
+                  <input 
+                  onChange={e => setName(e.target.value)}
+                  value={name}
+                  className='bg-transparent outline-none' 
                   type="text" placeholder="Full Name" required/>
                 </div>
               )
@@ -38,14 +44,20 @@ const Login = () => {
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
             rounded-full bg-[#333A5C]'>
               <img src={assets.mail_icon} alt="" />
-              <input className='bg-transparent outline-none' 
+              <input
+              onChange={e => setEmail(e.target.value)}
+              value={email} 
+              className='bg-transparent outline-none' 
               type="email" placeholder="Email id" required/>
             </div>
 
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
             rounded-full bg-[#333A5C]'>
               <img src={assets.lock_icon} alt="" />
-              <input className='bg-transparent outline-none' 
+              <input 
+              onChange={e => setPassword(e.target.value)}
+              value={password}
+              className='bg-transparent outline-none' 
               type="password" placeholder="Password" required/>
             </div>
 
