5285769f490cc2889f5d1084448ba445b8084bfd
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-26 13:47:26 +0500

    Only show the full name input when the form is in the sign up mode

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,12 +24,16 @@ const Login = () => {
           </p>
 
           <form>
-            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
-            rounded-full bg-[#333A5C]'>
-              <img src={assets.person_icon} alt="" />
-              <input className='bg-transparent outline-none' 
-              type="text" placeholder="Full Name" required/>
-            </div>
+            {
+              state === 'Sign Up' && (
+                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+                rounded-full bg-[#333A5C]'>
+                  <img src={assets.person_icon} alt="" />
+                  <input className='bg-transparent outline-none' 
+                  type="text" placeholder="Full Name" required/>
+                </div>
+              )
+            }
 
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
             rounded-full bg-[#333A5C]'>
