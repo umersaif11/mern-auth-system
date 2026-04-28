dee7a6a932b48ebf2d3d300193a677eeb6ceac61
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-28 19:04:00 +0500

    Use the useState hook to get user data and logged in info

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,12 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 export const AppContent = createContext()
 
 export const AppContextProvider = (props) => {
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [userData, setUserData] = useState(false)
 
     const value = {
 
