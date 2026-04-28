33a886f5b21b09eb9b8f75366d4b12d79ba96d54
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-04-28 09:53:05 +0500

    Create context and then create context provider

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,4 +2,12 @@ import { createContext } from "react"
 
 export const AppContent = createContext()
 
-export const
+export const AppContextProvider = (props) => {
+
+
+    return (
+        <AppContent.Provider value={}>
+            {props.children}
+        </AppContent.Provider>
+    )
+}
