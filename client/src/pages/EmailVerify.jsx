498f6dff8591ab3bc46cea2d5ab840fbde9663b5
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-02 19:45:10 +0500

    Create a logo within the email verify page

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const EmailVerify = () => {
+
+  const navigate = useNavigate()
   return (
     <div>
-        <h1>Email Verify</h1>
+        <div
+        className='flex justify-center items-center min-h-screen
+        text-white bg-linear-to-br from-blue-200 to-purple-400'>
+          <img
+                  onClick={() => navigate('/')} 
+                  src={assets.logo} alt="" className='absolute left-5 sm:left-20
+                  top-5 w-28 sm:w-32 cursor-pointer'/>
+
+        </div>
     </div>
   )
 }
