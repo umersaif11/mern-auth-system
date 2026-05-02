e2c5c9bee05955dc0501d77fd20e7a7176c26a26
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-02 16:03:23 +0500

    Get global variables from AppContext and if userdata is available then show a div otherwise show the button as before

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContent } from '../context/AppContext'
 
 const Navbar = () => {
 
   const navigate = useNavigate()
+  const {userData, backendUrl, setUserData, setIsLoggedin} = useContext(AppContent)
 
   return (
     <div
@@ -15,13 +17,19 @@ const Navbar = () => {
         alt=""
         className='w-28 sm:w-32'
         />
-        <button
-        onClick={() => navigate('/login')}
-        className='flex items-center gap-2 border border-gray-500
-        rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100
-        transition-all'>
+        {
+          userData ?
+          <div>
+            
+          </div> :
+          <button
+          onClick={() => navigate('/login')}
+          className='flex items-center gap-2 border border-gray-500
+          rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100
+          transition-all'>
             Login <img src={assets.arrow_icon} alt="" />
-        </button>
+          </button>
+        }
     </div>
   )
 }
