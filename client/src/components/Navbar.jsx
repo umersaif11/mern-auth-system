19d827be388b5494ec2af6c46787cfa54adabeed
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-02 17:53:56 +0500

    On hovering the icon it should give two items of verifying email and logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,6 +22,13 @@ const Navbar = () => {
           <div className='w-8 h-8 flex justify-center items-center rounded-full
           bg-black text-white relative group'>
             {userData.name[0].toUpperCase()}
+            <div className='absolute hidden group-hover:block top-0 right-0
+            z-10 text-black rounded pt-10'>
+              <ul>
+                <li>Verify email</li>
+                <li>Logout</li>
+              </ul>
+            </div>
           </div> :
           <button
           onClick={() => navigate('/login')}
