08bf22ffc9742437054f31197be7417375f65ba8
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-03 13:57:52 +0500

    Enter form and within it add heading and paragraph tags along with classnames for styling

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -11,10 +11,17 @@ const EmailVerify = () => {
         className='flex justify-center items-center min-h-screen
         text-white bg-linear-to-br from-blue-200 to-purple-400'>
           <img
-                  onClick={() => navigate('/')} 
-                  src={assets.logo} alt="" className='absolute left-5 sm:left-20
-                  top-5 w-28 sm:w-32 cursor-pointer'/>
-
+          onClick={() => navigate('/')} 
+          src={assets.logo} alt="" className='absolute left-5 sm:left-20
+          top-5 w-28 sm:w-32 cursor-pointer'/>
+          <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+            <h1 className='text-white text-2xl font-semibold text-center mb-4'>
+              Email Verify OTP
+            </h1>
+            <p className='text-center mb-6 text-indigo-300'>
+              Enter 6-digit code sent to your email id.
+            </p>
+          </form>
         </div>
     </div>
   )
