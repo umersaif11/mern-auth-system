1aeb83316bf63f7c2c0b3a987dfecd91821b7416
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-03 15:17:02 +0500

    Create a six input fields by looping over creating six input array

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -21,6 +21,15 @@ const EmailVerify = () => {
             <p className='text-center mb-6 text-indigo-300'>
               Enter 6-digit code sent to your email id.
             </p>
+            <div className='flex justify-between mb-8'>
+              {
+                Array(6).fill(0).map((_ , index) => (
+                  <input type='text' maxLength='1' key={index} required
+                  className='w-12 h-12 bg-[#333A5C] text-white text-center
+                  text-xl rounded-md'/>
+                ))
+              }
+            </div>
           </form>
         </div>
     </div>
