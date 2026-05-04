37aa40834ccae6b308b170f035e070c1e7081502
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-04 08:50:48 +0500

    Copy the otp from clipboard and then put it in the  six input fields

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -18,6 +18,16 @@ const EmailVerify = () => {
     }
   }
 
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData('text')
+    const pasteArray = paste.split('')
+    pasteArray.forEach((char, index) => {
+      if(inputRefs.current[index]) {
+        inputRefs.current[index].value = char
+      }
+    })
+  }
+
   const navigate = useNavigate()
   return (
     <div>
