79788c6313573139aab5f1a5ee529661f0d32aaa
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-07 08:55:56 +0500

    Add background color gradient and logo

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const ResetPassword = () => {
   return (
-    <div>
-        <h1>Reset Password</h1>
+    <div
+    className='flex justify-center items-center min-h-screen
+    text-white bg-linear-to-br from-blue-200 to-purple-400'>
+        <img
+        onClick={() => navigate('/')} 
+        src={assets.logo} alt="" className='absolute left-5 sm:left-20
+        top-5 w-28 sm:w-32 cursor-pointer'/>
     </div>
   )
 }
