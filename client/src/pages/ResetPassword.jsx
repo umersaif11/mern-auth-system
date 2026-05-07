3971210f08ec8e937685e8d660a9f611cec510af
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-07 09:07:47 +0500

    Add div and within it icon and input email

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const ResetPassword = () => {
 
@@ -18,11 +19,18 @@ const ResetPassword = () => {
         <form 
         className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
           <h1 className='text-white text-2xl font-semibold text-center mb-4'>
-            Email Verify OTP
+            Reset Password
           </h1>
           <p className='text-center mb-6 text-indigo-300'>
-            Enter 6-digit code sent to your email id.
+            Enter your registered email address
           </p>
+
+          <div
+          className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+          rounded-full bg-[#333A5C]'>
+            <img src={assets.mail_icon} alt="" className='w-3 h-3'/>
+            <input type="email" />
+          </div>
 
         </form>
     </div>
