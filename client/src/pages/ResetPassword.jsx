3c42d59fca6c8b94680c2ab37c9e12561fa79cc9
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-07 09:01:14 +0500

    Enter form and within it paragraph and heading

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const ResetPassword = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div
     className='flex justify-center items-center min-h-screen
@@ -10,6 +13,18 @@ const ResetPassword = () => {
         onClick={() => navigate('/')} 
         src={assets.logo} alt="" className='absolute left-5 sm:left-20
         top-5 w-28 sm:w-32 cursor-pointer'/>
+
+        {/* Enter Email ID */}
+        <form 
+        className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+          <h1 className='text-white text-2xl font-semibold text-center mb-4'>
+            Email Verify OTP
+          </h1>
+          <p className='text-center mb-6 text-indigo-300'>
+            Enter 6-digit code sent to your email id.
+          </p>
+
+        </form>
     </div>
   )
 }
