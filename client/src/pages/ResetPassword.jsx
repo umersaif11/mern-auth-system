293b7bf96e9e768f42195cbe2363ce4a7d29153e
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-08 16:04:49 +0500

    If email is sent and otp is submitted then show the third form

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -106,28 +106,31 @@ const ResetPassword = () => {
         }
 
         {/* Enter new password */}
-        <form 
-        className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
-          <h1 className='text-white text-2xl font-semibold text-center mb-4'>
-            New Password
-          </h1>
-          <p className='text-center mb-6 text-indigo-300'>
-            Enter the new password below
-          </p>
-          <div
-          className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
-          rounded-full bg-[#333A5C]'>
-            <img src={assets.lock_icon} alt="" className='w-3 h-3'/>
-            <input type="password" placeholder='Password'
-            className='bg-transparent outline-none text-white'
-            value={newPassword} onChange={e => setNewPassword(e.target.value)}/>
-          </div>
-          <button
-          className='w-full py-2.5 bg-linear-to-r from-indigo-500
-          to-indigo-900 text-white rounded-full mt-3'>
-            Submit
-          </button>
-        </form>
+        {
+          isOtpSubmitted && isEmailSent &&
+          <form 
+          className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+            <h1 className='text-white text-2xl font-semibold text-center mb-4'>
+              New Password
+            </h1>
+            <p className='text-center mb-6 text-indigo-300'>
+              Enter the new password below
+            </p>
+            <div
+            className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
+            rounded-full bg-[#333A5C]'>
+              <img src={assets.lock_icon} alt="" className='w-3 h-3'/>
+              <input type="password" placeholder='Password'
+              className='bg-transparent outline-none text-white'
+              value={newPassword} onChange={e => setNewPassword(e.target.value)}/>
+            </div>
+            <button
+            className='w-full py-2.5 bg-linear-to-r from-indigo-500
+            to-indigo-900 text-white rounded-full mt-3'>
+              Submit
+            </button>
+          </form>
+        }
     </div>
   )
 }
