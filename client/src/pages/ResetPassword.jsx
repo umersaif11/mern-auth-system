4a1f80f32ec8f16865fa488c03b262a623bd8b0f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-05-08 10:56:07 +0500

    Copy paste the form , handle input, handle keydown and handle paste functions from EmailVerify.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -9,6 +9,30 @@ const ResetPassword = () => {
 
   const [email, setEmail] = useState('')
 
+  const inputRefs = useRef([])
+  
+  const handleInput = (e, index) => {
+    if(e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus()
+    }
+  }
+
+  const handleKeyDown = (e, index) => {
+    if(e.key === 'Backspace' && e.target.value === '' && index > 0) {
+      inputRefs.current[index - 1].focus()
+    }
+  }
+
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData('text')
+    const pasteArray = paste.split('')
+    pasteArray.forEach((char, index) => {
+      if(inputRefs.current[index]) {
+        inputRefs.current[index].value = char
+      }
+    })
+  }
+
   return (
     <div
     className='flex justify-center items-center min-h-screen
@@ -27,7 +51,6 @@ const ResetPassword = () => {
           <p className='text-center mb-6 text-indigo-300'>
             Enter your registered email address
           </p>
-
           <div
           className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
           rounded-full bg-[#333A5C]'>
@@ -36,7 +59,40 @@ const ResetPassword = () => {
             className='bg-transparent outline-none text-white'
             value={email} onChange={e => setEmail(e.target.value)}/>
           </div>
+          <button
+          className='w-full py-2.5 bg-linear-to-r from-indigo-500
+          to-indigo-900 text-white rounded-full mt-3'>
+            Submit
+          </button>
+        </form>
 
+        {/* OTP input form */}
+        <form
+        onSubmit={} 
+        className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+          <h1 className='text-white text-2xl font-semibold text-center mb-4'>
+            Reset password OTP
+          </h1>
+          <p className='text-center mb-6 text-indigo-300'>
+            Enter 6-digit code sent to your email id.
+          </p>
+          <div className='flex justify-between mb-8' onPaste={handlePaste}>
+            {
+              Array(6).fill(0).map((_ , index) => (
+                <input type='text' maxLength='1' key={index} required
+                className='w-12 h-12 bg-[#333A5C] text-white text-center
+                text-xl rounded-md'
+                ref={element => inputRefs.current[index] = element}
+                onInput={e => handleInput(e, index)}
+                onKeyDown={e => handleKeyDown(e, index)}/>
+              ))
+            }
+          </div>
+          <button 
+          className='w-full py-2.5 bg-linear-to-r from-indigo-500 to-indigo-900
+          text-white rounded-full'>
+            Verify email
+          </button>
         </form>
     </div>
   )
